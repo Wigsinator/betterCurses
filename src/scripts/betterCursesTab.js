c3a89acb701e0bfb85015f8d1f8c02c37209838f
Wigsinator <wigsinator@gmail.com>
2020-09-17 00:53:46 -0400

    Add Curse tab to items
    Currently has issue where text boxes are not visually represented correctly.
    They still exist, can be highlighted and filled visually, but they are invisible otherwise

---
+++ src/scripts/betterCursesTab.js
@@ -0,0 +1,51 @@
+var makeActive = false;
+
+export function initSheetTab() {
+  let itemSheets = {}
+  Object.values(CONFIG.Item.sheetClasses).forEach(itemType => Object.keys(itemType).forEach(sheetName => itemSheets[sheetName] = 1));
+  Object.keys(itemSheets).forEach(
+    sheetName => Hooks.on(`render${sheetName.split(".")[1]}`, (app, html, data) => {
+        addBetterCursesTab(app, html, data);
+    })
+  );
+}
+
+async function addBetterCursesTab(app,html,data) {
+  let item = app.object;
+  if (["loot"].includes(item.type)) return;
+
+  // Define the properties for curse items
+  if (!hasProperty(item.data, "flags.betterCurses.isCurse")) setProperty(item.data, "flags.betterCurses.isCurse", false);
+  if (!hasProperty(item.data, "flags.betterCurses.curseName")) setProperty(item.data, "flags.betterCurses.curseName", "");
+  if (!hasProperty(item.data, "flags.betterCurses.formula")) setProperty(item.data, "flags.betterCurses.formula", "");
+  if (!hasProperty(item.data, "flags.betterCurses.mw")) setProperty(item.data, "flags.betterCurses.mw", false);
+  if (!hasProperty(item.data, "flags.betterCurses.rw")) setProperty(item.data, "flags.betterCurses.rw", false);
+  if (!hasProperty(item.data, "flags.betterCurses.ms")) setProperty(item.data, "flags.betterCurses.ms", false);
+  if (!hasProperty(item.data, "flags.betterCurses.rs")) setProperty(item.data, "flags.betterCurses.rs", false);
+
+  // Create the tab
+  let tabSelector = html.find(`.tabs`),
+    settingsContainer = html.find(`.sheet-body`),
+    betterCursesTabString = `<a class="item" data-tab="betterCurses">${game.i18n.localize("betterCurses.Title")}</a>`,
+    tab = tabSelector.append($(betterCursesTabString));
+
+  if (settingsContainer.length === 0) settingsContainer = html.find(`.primary-body`);
+
+  let betterCursesTabTemplate = await renderTemplate("./modules/betterCurses/templates/betterCurses-tab.html", {
+    flags: item.data.flags
+  });
+  let extraTab = settingsContainer.append(betterCursesTabTemplate);
+
+  if (makeActive && app._tabs[0]) app._tabs[0].activate("betterCurses");
+  makeActive = false;
+  
+   // intercept Input Click and set Tab
+  html.find('.betterCurses input[type="checkbox"]').click(ev => {
+    makeActive = true;
+  });
+
+  html.find('.betterCurses input[type="text"]').click(ev => {
+    makeActive = true;
+  });
+
+}
