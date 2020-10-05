2d81999cf7b4172ae27c192fa4a76b6b22e20bd3
Wigsinator <wigsinator@gmail.com>
2020-10-05 01:24:33 -0400

    Correct Hook setup
    Separate setupControlHook and setupTargetHook
    Fix setupTargetHook so that it works correctly

--- src/scripts/setupHooks.js
+++ src/scripts/setupHooks.js
@@ -1,24 +1,29 @@
-export function setupHooks(){
-  Hooks.on("targetToken", handleTarget => function() {
-    let targets = game.user.targets.ids;
-    let controlledTokens = canvas.tokens.controlled
-    if (targets.length == 1){
-      for (controlledToken of controlledTokens){
-        applyEffects(controlledToken.actor, canvas.tokens.get(targets[0]).actor)
-      }
-    } else {
-      for (controlledToken of controlledTokens){
-        clearEffects(controlToken.actor)
-      }
-    }
-  })
-  Hooks.on("controlToken", (controlledToken, selected) => {
+export function setupControlHook(){
+  return Hooks.on("controlToken", (controlledToken, selected) => {
     if (!selected) {
       clearEffects(controlledToken.actor);
     } else {
       let targets = game.user.targets.ids;
       if (targets.length == 1){
         applyEffects(controlledToken.actor, canvas.tokens.get(targets[0]).actor)
+      }
+    }
+  })
+}
+
+export function setupTargetHook(){
+  return Hooks.on("targetToken", handleTarget => {
+    console.log("betterCurses | Token Target changed")
+    let targets = game.user.targets.ids;
+    let controlledTokens = canvas.tokens.controlled
+    console.log(controlledTokens)
+    if (targets.length == 1){
+      for (var controlledToken of controlledTokens){
+        applyEffects(controlledToken.actor, canvas.tokens.get(targets[0]).actor)
+      }
+    } else {
+      for (var controlledToken of controlledTokens){
+        clearEffects(controlledToken.actor)
       }
     }
   })
