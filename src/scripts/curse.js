41cbba55bf8c156396c6f127d87d5c8a32c591db
Wigsinator <wigsinator@gmail.com>
2020-10-21 20:28:31 -0400

    Make setFlag asynchronous

--- src/scripts/curse.js
+++ src/scripts/curse.js
@@ -2,12 +2,12 @@ import { log } from "./helpers.js"
 import { setFlag } from "./setFlag.js"
 import { applyEffects } from "./applyEffects.js"
 
-export function curse(curse){
+export async function curse(curse){
     let user = game.user;
     let selectedId = ChatMessage.getSpeaker().token;
 
     for (let targetId of user.targets.ids){
-        setFlag(targetId, selectedId, curse)
+        await setFlag(targetId, selectedId, curse)
     }
     if (user.targets.ids.length === 1){
         applyEffects(canvas.tokens.get(selectedId).actor, canvas.tokens.get(targets[0]).actor)
