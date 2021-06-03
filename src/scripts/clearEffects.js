16612f460d26ad798983859b316ee5cf6afddb7f
william <wigsinator@gmail.com>
2021-06-02 21:08:30 -0400

    Update clearEffects to use new terminology

--- src/scripts/clearEffects.js
+++ src/scripts/clearEffects.js
@@ -1,15 +1,6 @@
 import { log } from "./helpers.js"
 
 export async function clearEffects(actor){
-  var filtered = actor.effects.filter(x => x.data.label === "betterCurses");
-  var promiseList = [];
-  for (var effect of filtered) {
-    log(`Clearing Effect ${effect.id}`);
-    promiseList.push(actor.deleteEmbeddedEntity("ActiveEffect", effect.id));
-  }
-  if (promiseList) {
-    log("Promise List:")
-    log(promiseList);
-    await Promise.all(promiseList);
-  }
+  var filteredIds = actor.effects.filter(x => x.data.label === "betterCurses").map(x => x.id);
+  await actor.deleteEmbeddedDocuments("ActiveEffect", filteredIds);
 }
