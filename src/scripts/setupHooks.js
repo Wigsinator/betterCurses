90da5a1e1d496158cb5cbbae41e05337e7305b87
william <wigsinator@gmail.com>
2021-06-02 21:27:55 -0400

    Change the way hooks interacts with targets list

--- src/scripts/setupHooks.js
+++ src/scripts/setupHooks.js
@@ -6,9 +6,9 @@ export function setupControlHook(){
   return Hooks.on("controlToken", (controlledToken, selected) => {
     if (GetAllFlags(controlledToken, 'multilevel-tokens')) return;
     clearEffects(controlledToken.actor).then(() => {
-      let targets = game.user.targets.ids;
-      if (selected && targets.length == 1) {
-        applyEffects(controlledToken.actor, canvas.tokens.get(targets[0]).actor);
+      let targetIds = Array.from(game.user.targets, t=> t.id);
+      if (selected && targetIds.length == 1) {
+        applyEffects(controlledToken.actor, canvas.tokens.get(targetIds[0]).actor);
       }
     });
   })
@@ -17,13 +17,13 @@ export function setupControlHook(){
 export function setupTargetHook(){
   return Hooks.on("targetToken", handleTarget => {
     log(`Token Target changed`)
-    let targets = game.user.targets.ids;
+    let targetIds = Array.from(game.user.targets, t=> t.id);
     let controlledTokens = canvas.tokens.controlled;
     log(controlledTokens);
-    if (targets.length == 1){
+    if (targetIds.length == 1){
       for (var controlledToken of controlledTokens){
         if (GetAllFlags(controlledToken, 'multilevel-tokens')) continue;
-        applyEffects(controlledToken.actor, canvas.tokens.get(targets[0]).actor);
+        applyEffects(controlledToken.actor, canvas.tokens.get(targetIds[0]).actor);
       }
     } else {
       for (var controlledToken of controlledTokens){
